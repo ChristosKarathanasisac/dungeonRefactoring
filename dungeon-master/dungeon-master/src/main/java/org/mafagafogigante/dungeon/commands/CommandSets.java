0257600e828b2db493a5bc13c68a8d43e0d24605
Christos Karathanasis <christoskarathanasisac@gmail.com>
2023-02-05 21:33:53 +0200

    initializeDebugCommandSet of CommandSets (Long Method) Refactoring. Method extracted to 10 sub-methods

--- dungeon-master/dungeon-master/src/main/java/org/mafagafogigante/dungeon/commands/CommandSets.java
+++ dungeon-master/dungeon-master/src/main/java/org/mafagafogigante/dungeon/commands/CommandSets.java
@@ -320,70 +320,139 @@ final class CommandSets {
 
   private static CommandSet initializeDebugCommandSet() {
     CommandSet commandSet = CommandSet.emptyCommandSet();
-    commandSet.addCommand(new Command("achievements", "Writes the achievements you have not yet unlocked.") {
+    commandSet.addCommand(commandUnlockedAchievements());
+    commandSet.addCommand(commandExploration());
+    commandSet.addCommand(commandKills());
+    commandSet.addCommand(commandCurrentLocation());
+    commandSet.addCommand(commandGetMap());
+    commandSet.addCommand(commandGetItems());
+    commandSet.addCommand(commandCheckIfGameIsSaved());
+    commandSet.addCommand(commandCreateCreature());
+    commandSet.addCommand(commandGetCurrentTime());
+    commandSet.addCommand(commandMakeTimePass());
+    return commandSet;
+  }
+
+/**
+ * @return
+ */
+private static Command commandMakeTimePass() {
+	return new Command("wait", "Makes time pass.") {
       @Override
       public void execute(@NotNull String[] arguments) {
-        AchievementTracker tracker = Game.getGameState().getHero().getAchievementTracker();
-        List<Achievement> notYetUnlockedAchievementList = new ArrayList<>();
-        for (Achievement achievement : AchievementStoreFactory.getDefaultStore().getAchievements()) {
-          if (tracker.hasNotBeenUnlocked(achievement)) {
-            notYetUnlockedAchievementList.add(achievement);
-          }
-        }
-        if (notYetUnlockedAchievementList.isEmpty()) {
-          Writer.write("All achievements have been unlocked.");
-        } else {
-          Collections.sort(notYetUnlockedAchievementList, new Comparator<Achievement>() {
-            @Override
-            public int compare(Achievement o1, Achievement o2) {
-              return o1.getName().compareTo(o2.getName());
+        DebugWaitParser.parseDebugWait(arguments);
+      }
+    };
+}
+
+/**
+ * @return
+ */
+private static Command commandGetCurrentTime() {
+	return new Command("time", "Writes information about the current time.") {
+      @Override
+      public void execute(@NotNull String[] arguments) {
+        Writer.write(Game.getGameState().getWorld().getWorldDate().toString());
+      }
+    };
+}
+
+/**
+ * @return
+ */
+private static Command commandCreateCreature() {
+	return new Command("spawn", "Spawns a creature.") {
+      @Override
+      public void execute(@NotNull String[] arguments) {
+        if (arguments.length != 0) {
+          for (String argument : arguments) {
+            Id givenId = new Id(argument.toUpperCase(Locale.ENGLISH));
+            World world = Game.getGameState().getWorld();
+            Creature creature = world.getCreatureFactory().makeCreature(givenId, world);
+            if (creature != null) {
+              Game.getGameState().getHero().getLocation().addCreature(creature);
+              Writer.write("Spawned a " + creature.getName() + ".");
+              Engine.refresh(); // Set the game state to unsaved after adding a creature to the world.
+            } else {
+              Writer.write(givenId + " does not match any known creature.");
             }
-          });
-          for (Achievement achievement : notYetUnlockedAchievementList) {
-            Writer.write(String.format("%s : %s", achievement.getName(), achievement.getInfo()));
           }
-        }
-      }
-    });
-    commandSet.addCommand(new Command("exploration", "Writes statistics about your exploration.") {
-      @Override
-      public void execute(@NotNull String[] arguments) {
-        ExplorationStatistics explorationStatistics = Game.getGameState().getStatistics().getExplorationStatistics();
-        List<ColumnAlignment> columnAlignments = new ArrayList<>();
-        columnAlignments.add(ColumnAlignment.LEFT);
-        columnAlignments.add(ColumnAlignment.RIGHT);
-        columnAlignments.add(ColumnAlignment.RIGHT);
-        columnAlignments.add(ColumnAlignment.RIGHT);
-        Table table = new Table("Name", "Kills", "Visited so far", "Maximum number of visits");
-        table.setColumnAlignments(columnAlignments);
-        for (LocationPreset preset : LocationPresetStore.getDefaultLocationPresetStore().getAllPresets()) {
-          String name = preset.getName().getSingular();
-          String kills = String.valueOf(explorationStatistics.getKillCount(preset.getId()));
-          String visitedSoFar = String.valueOf(explorationStatistics.getVisitedLocations(preset.getId()));
-          String maximumNumberOfVisits = String.valueOf(explorationStatistics.getMaximumNumberOfVisits(preset.getId()));
-          table.insertRow(name, kills, visitedSoFar, maximumNumberOfVisits);
-        }
-        Writer.write(table);
-      }
-    });
-    commandSet.addCommand(new Command("kills", "Writes statistics about your killings.") {
-      @Override
-      public void execute(@NotNull String[] arguments) {
-        CounterMap<CauseOfDeath> map =
-            Game.getGameState().getStatistics().getBattleStatistics().getKillsByCauseOfDeath();
-        if (map.isNotEmpty()) {
-          Table table = new Table("Type", "Count");
-          table.setColumnAlignments(Arrays.asList(ColumnAlignment.LEFT, ColumnAlignment.RIGHT));
-          for (CauseOfDeath causeOfDeath : map.keySet()) {
-            table.insertRow(causeOfDeath.toString(), String.valueOf(map.getCounter(causeOfDeath)));
-          }
-          Writer.write(table);
         } else {
-          Writer.write("You haven't killed anything yet. Go kill something!");
+          Messenger.printMissingArgumentsMessage();
         }
       }
-    });
-    commandSet.addCommand(new Command("location", "Writes information about the current location.") {
+    };
+}
+
+/**
+ * @return
+ */
+private static Command commandCheckIfGameIsSaved() {
+	return new Command("saved", "Tests if the game is saved or not.") {
+      @Override
+      public void execute(@NotNull String[] arguments) {
+        if (Game.getGameState().isSaved()) {
+          Writer.write("The game is saved.");
+        } else {
+          Writer.write("This game state is not saved.");
+        }
+      }
+    };
+}
+
+/**
+ * @return
+ */
+private static Command commandGetItems() {
+	return new Command("give", "Gives items to the character.") {
+      @Override
+      public void execute(@NotNull String[] arguments) {
+        if (arguments.length != 0) {
+          World world = Game.getGameState().getWorld();
+          Date date = world.getWorldDate();
+          try {
+            Id id = new Id(arguments[0].toUpperCase(Locale.ENGLISH));
+            if (world.getItemFactory().canMakeItem(id)) {
+              Item item = world.getItemFactory().makeItem(id, date);
+              Writer.write("Item successfully created.");
+              Hero hero = Game.getGameState().getHero();
+              if (hero.getInventory().simulateItemAddition(item) == SimulationResult.SUCCESSFUL) {
+                hero.addItem(item);
+              } else {
+                hero.getLocation().addItem(item);
+                Writer.write("Item could not be added to your inventory. Thus, it was added to the current location.");
+              }
+              Engine.refresh(); // Set the game state to unsaved after adding an item to the world.
+            } else {
+              Writer.write("Item could not be created due to a restriction.");
+            }
+          } catch (IllegalArgumentException invalidPreset) {
+            Writer.write("Item could not be created.");
+          }
+        } else {
+          Messenger.printMissingArgumentsMessage();
+        }
+      }
+    };
+}
+
+/**
+ * @return
+ */
+private static Command commandGetMap() {
+	return new Command("map", "Produces a map as complete as possible.") {
+      @Override
+      public void execute(@NotNull String[] arguments) {
+        WorldMapWriter.writeDebugMap();
+      }
+    };
+}
+
+/**
+ * @return
+ */
+private static Command commandCurrentLocation() {
+	return new Command("location", "Writes information about the current location.") {
       @Override
       public void execute(@NotNull String[] arguments) {
         final int width = 40;  // The width of the row's "tag".
@@ -420,88 +489,89 @@ final class CommandSets {
         dungeonString.append("\n");
         Writer.write(dungeonString);
       }
-    });
-    commandSet.addCommand(new Command("map", "Produces a map as complete as possible.") {
+    };
+}
+
+/**
+ * @return
+ */
+private static Command commandKills() {
+	return new Command("kills", "Writes statistics about your killings.") {
       @Override
       public void execute(@NotNull String[] arguments) {
-        WorldMapWriter.writeDebugMap();
-      }
-    });
-    commandSet.addCommand(new Command("give", "Gives items to the character.") {
-      @Override
-      public void execute(@NotNull String[] arguments) {
-        if (arguments.length != 0) {
-          World world = Game.getGameState().getWorld();
-          Date date = world.getWorldDate();
-          try {
-            Id id = new Id(arguments[0].toUpperCase(Locale.ENGLISH));
-            if (world.getItemFactory().canMakeItem(id)) {
-              Item item = world.getItemFactory().makeItem(id, date);
-              Writer.write("Item successfully created.");
-              Hero hero = Game.getGameState().getHero();
-              if (hero.getInventory().simulateItemAddition(item) == SimulationResult.SUCCESSFUL) {
-                hero.addItem(item);
-              } else {
-                hero.getLocation().addItem(item);
-                Writer.write("Item could not be added to your inventory. Thus, it was added to the current location.");
-              }
-              Engine.refresh(); // Set the game state to unsaved after adding an item to the world.
-            } else {
-              Writer.write("Item could not be created due to a restriction.");
-            }
-          } catch (IllegalArgumentException invalidPreset) {
-            Writer.write("Item could not be created.");
+        CounterMap<CauseOfDeath> map =
+            Game.getGameState().getStatistics().getBattleStatistics().getKillsByCauseOfDeath();
+        if (map.isNotEmpty()) {
+          Table table = new Table("Type", "Count");
+          table.setColumnAlignments(Arrays.asList(ColumnAlignment.LEFT, ColumnAlignment.RIGHT));
+          for (CauseOfDeath causeOfDeath : map.keySet()) {
+            table.insertRow(causeOfDeath.toString(), String.valueOf(map.getCounter(causeOfDeath)));
           }
+          Writer.write(table);
         } else {
-          Messenger.printMissingArgumentsMessage();
+          Writer.write("You haven't killed anything yet. Go kill something!");
         }
       }
-    });
-    commandSet.addCommand(new Command("saved", "Tests if the game is saved or not.") {
+    };
+}
+
+/**
+ * @return
+ */
+private static Command commandExploration() {
+	return new Command("exploration", "Writes statistics about your exploration.") {
       @Override
       public void execute(@NotNull String[] arguments) {
-        if (Game.getGameState().isSaved()) {
-          Writer.write("The game is saved.");
-        } else {
-          Writer.write("This game state is not saved.");
+        ExplorationStatistics explorationStatistics = Game.getGameState().getStatistics().getExplorationStatistics();
+        List<ColumnAlignment> columnAlignments = new ArrayList<>();
+        columnAlignments.add(ColumnAlignment.LEFT);
+        columnAlignments.add(ColumnAlignment.RIGHT);
+        columnAlignments.add(ColumnAlignment.RIGHT);
+        columnAlignments.add(ColumnAlignment.RIGHT);
+        Table table = new Table("Name", "Kills", "Visited so far", "Maximum number of visits");
+        table.setColumnAlignments(columnAlignments);
+        for (LocationPreset preset : LocationPresetStore.getDefaultLocationPresetStore().getAllPresets()) {
+          String name = preset.getName().getSingular();
+          String kills = String.valueOf(explorationStatistics.getKillCount(preset.getId()));
+          String visitedSoFar = String.valueOf(explorationStatistics.getVisitedLocations(preset.getId()));
+          String maximumNumberOfVisits = String.valueOf(explorationStatistics.getMaximumNumberOfVisits(preset.getId()));
+          table.insertRow(name, kills, visitedSoFar, maximumNumberOfVisits);
         }
+        Writer.write(table);
       }
-    });
-    commandSet.addCommand(new Command("spawn", "Spawns a creature.") {
+    };
+}
+
+/**
+ * @return
+ */
+private static Command commandUnlockedAchievements() {
+	return new Command("achievements", "Writes the achievements you have not yet unlocked.") {
       @Override
       public void execute(@NotNull String[] arguments) {
-        if (arguments.length != 0) {
-          for (String argument : arguments) {
-            Id givenId = new Id(argument.toUpperCase(Locale.ENGLISH));
-            World world = Game.getGameState().getWorld();
-            Creature creature = world.getCreatureFactory().makeCreature(givenId, world);
-            if (creature != null) {
-              Game.getGameState().getHero().getLocation().addCreature(creature);
-              Writer.write("Spawned a " + creature.getName() + ".");
-              Engine.refresh(); // Set the game state to unsaved after adding a creature to the world.
-            } else {
-              Writer.write(givenId + " does not match any known creature.");
-            }
+        AchievementTracker tracker = Game.getGameState().getHero().getAchievementTracker();
+        List<Achievement> notYetUnlockedAchievementList = new ArrayList<>();
+        for (Achievement achievement : AchievementStoreFactory.getDefaultStore().getAchievements()) {
+          if (tracker.hasNotBeenUnlocked(achievement)) {
+            notYetUnlockedAchievementList.add(achievement);
           }
+        }
+        if (notYetUnlockedAchievementList.isEmpty()) {
+          Writer.write("All achievements have been unlocked.");
         } else {
-          Messenger.printMissingArgumentsMessage();
+          Collections.sort(notYetUnlockedAchievementList, new Comparator<Achievement>() {
+            @Override
+            public int compare(Achievement o1, Achievement o2) {
+              return o1.getName().compareTo(o2.getName());
+            }
+          });
+          for (Achievement achievement : notYetUnlockedAchievementList) {
+            Writer.write(String.format("%s : %s", achievement.getName(), achievement.getInfo()));
+          }
         }
       }
-    });
-    commandSet.addCommand(new Command("time", "Writes information about the current time.") {
-      @Override
-      public void execute(@NotNull String[] arguments) {
-        Writer.write(Game.getGameState().getWorld().getWorldDate().toString());
-      }
-    });
-    commandSet.addCommand(new Command("wait", "Makes time pass.") {
-      @Override
-      public void execute(@NotNull String[] arguments) {
-        DebugWaitParser.parseDebugWait(arguments);
-      }
-    });
-    return commandSet;
-  }
+    };
+}
 
   static boolean hasCommandSet(String identifier) {
     return commandSetMap.containsKey(identifier);
